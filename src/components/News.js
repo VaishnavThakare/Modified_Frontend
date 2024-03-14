7fc04220daddc4674ccb757ee71b914ccbbe8edb
maneSidd <siddharth.mane@mitaoe.ac.in>
2024-03-14 19:20:19 +0530

    News in landing page connected to backend

--- src/components/News.js
+++ src/components/News.js
@@ -2,51 +2,74 @@ import React, { useState } from 'react';
 import demo1 from './demo7.jpg';
 import demo2 from './demo2.jpg';
 import demo3 from './demo3.jpg';
+import axios from 'axios';
+import { useEffect } from 'react';
 
-const newsData = [
-  {
-    id: 1,
-    title: 'News Title 1',
-    image: demo1,
-    description: 'Lorem ipsum dolor sit amet,  Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  },
-  {
-    id: 2,
-    title: 'News Title 2',
-    image: demo2,
-    description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-  },
-  {
-    id: 3,
-    title: 'News Title 3',
-    image: demo3,
-    description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-  },
-];
 
 const News = () => {
+
   const [currentNewsIndex, setCurrentNewsIndex] = useState(0);
+  const [news, setnews] = useState([
+    {
+      id: 1,
+      title: 'News Title 1',
+      imagePath: demo1,
+      content: 'Lorem ipsum dolor sit amet,  Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    {
+      id: 2,
+      title: 'News Title 2',
+      imagePath: demo2,
+      content: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    },
+    {
+      id: 3,
+      title: 'News Title 3',
+      imagePath: demo3,
+      content: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    },
+  ]);
+
+
+  const getAllNews = async () => {
+    try {
+      let res = await axios.get(`${process.env.REACT_APP_API_URL}/News/All`);
+      if (res.status == 200 && res.data != null) {
+        setnews(res.data);
+      }
+    } 
+    catch (error) {
+      console.error("Error fetching Project data:", error);
+    }
+    finally{
+      console.log(news);
+    }
+  };
 
   const handleForwardClick = () => {
-    setCurrentNewsIndex((prevIndex) => (prevIndex + 1) % newsData.length);
+    setCurrentNewsIndex((prevIndex) => (prevIndex + 1) % news.length);
   };
 
   const handleBackwardClick = () => {
-    setCurrentNewsIndex((prevIndex) => (prevIndex - 1 + newsData.length) % newsData.length);
+    setCurrentNewsIndex((prevIndex) => (prevIndex - 1 + news.length) % news.length); 
   };
+
+  useEffect(()=>{
+    getAllNews();
+  },[]);
 
   return (
     <div className="container mr-10 relative  w-full">
-      <img src={newsData[currentNewsIndex].image} alt="" className='w-full h-40 mx-auto' />
-      <h1 className='text-center font-semibold'>{newsData[currentNewsIndex].title}</h1>
+      <img src={news[currentNewsIndex].imagePath} alt="" className='w-full h-40 mx-auto' />
+      <h1 className='text-center font-semibold'>{news[currentNewsIndex].title}</h1>
       <p className="text-1xl text-gray-800 mb-4 px-6 py-2">
-        {newsData[currentNewsIndex].description}
+        {news[currentNewsIndex].content}
       </p>
       <div className="absolute top-1/2 transform -translate-y-1/2 left-0" style={{ width: '100%' }}>
-        <button className="absolute left-0 top-1/2 transform -translate-y-1/2 text-2xl px-2 py-1 text-gray" onClick={handleBackwardClick}>
+        <button className="absolute left-0 top-[0px] text-2xl text-gray bg-white w-[22px] h-[35px] rounded" onClick={handleBackwardClick}>
           &lt; 
         </button>
-        <button className="absolute right-0 top-1/4 transform -translate-y-1/2 text-2xl px-2 py-1 text-gray" onClick={handleForwardClick}>
+        <button className="absolute right-0 top-[0px] text-2xl text-gray bg-white w-[22px] h-[35px] rounded" onClick={handleForwardClick}>
           &gt;
         </button>
       </div>
