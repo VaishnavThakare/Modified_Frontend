615e73ba759a8487af7475b5adf366b42895501f
maneSidd <siddharth.mane@mitaoe.ac.in>
2024-03-14 14:53:54 +0530

    All Events Updated

--- src/pages/admin/Event/AllEvent.js
+++ src/pages/admin/Event/AllEvent.js
@@ -5,6 +5,7 @@ export default function AllEvent() {
   const [events, setevents] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage] = useState(3);
+  const [modal, setmodal] = useState();
 
   const getAllEvents = async () => {
     try {
@@ -14,47 +15,34 @@ export default function AllEvent() {
       if(res.status == 200 && res.data!=null){
         data = res.data;
       }
-      else{
-        data = [
-          {
-              id:1,
-              isActive:true,
-              createdOn:new Date(),
-              lastModifiedOn:new Date(),
-              eventDateTime:new Date(),
-              imagePath:`${process.env.PUBLIC_URL}/Banners/1.jpg`,
-              title:"Event 1",
-              content:"content 1"
-          },
-          {
-              id:2,
-              isActive:true,
-              createdOn:new Date(),
-              lastModifiedOn:new Date(),
-              eventDateTime:new Date(),
-              imagePath:`${process.env.PUBLIC_URL}/Banners/2.jpg`,
-              title:"Event 2",
-              content:"content 2"
-          },
-          {
-              id:3,
-              isActive:true,
-              createdOn:new Date(),
-              lastModifiedOn:new Date(),
-              eventDateTime:new Date(),
-              imagePath:`${process.env.PUBLIC_URL}/Banners/3.jpg`,
-              title:"Event 3",
-              content:"content 3"
-          }
-        ] ;
-      }
-        console.log(data);
-        setevents(data);    
+      console.log(data);
+      setevents(data);    
 
     } catch (error) {
       console.error("Error fetching Project data:", error);
     }
   };
+
+  const handleChange =()=>{};
+
+  const handleDelete = async (id, index) => {
+    
+    try {
+        // console.log(events);
+        // console.log(eventId);
+        const response = await axios.delete(`${process.env.REACT_APP_API_URL}/Event/${id}`);
+        if (response.status === 200) 
+            alert("Banner Deleted");
+        getAllEvents();
+
+    } catch (error) {
+      console.error("Error :", error.message);
+    }
+  };
+
+  const handleEdit =()=>{};
+  
+  const toggleEditModal =()=>{};
 
 
   useEffect(() => {
@@ -70,7 +58,89 @@ export default function AllEvent() {
 
   return (
     <>
-      <div className="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 pr-10 lg:px-8 mb-8">
+
+<div className="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 pr-10 lg:px-8 mb-8">
+        <div className="align-middle inline-block min-w-full shadow overflow-hidden bg-white shadow-dashboard px-8 pt-3 rounded-bl-lg rounded-br-lg">
+          <div className="flex text-2xl font-bold text-gray-500 mb-4 justify-center items-center">
+            <h2>Events </h2>
+          </div>
+
+          {currentItems.map((event, index) => (
+            <div
+              key={event.id}
+              class="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-xl mx-auto border border-gray-400 bg-white mb-4"
+            >
+              <div class="w-full md:w-1/3 grid place-items-center">
+                <img src={event.imagePath} alt={event.title} class="rounded-xl" />
+              </div>
+              <div class="w-full md:w-2/3 flex flex-col space-y-2 p-3">
+                <div class="flex justify-between item-center">
+                  <div class="bg-gray-200 px-3 py-1 rounded-full text-xs font-medium text-gray-800 hidden md:block">
+                    {event.isActive ? "Active" : "Inactive"}
+                  </div>
+                </div>
+                <h3 class="font-black text-gray-800 md:text-lg text-lg">
+                  {event.title}
+                </h3>
+                <h3 class="font-black text-gray-800 md:text-sm text-md">
+                  {event.eventDateTime}
+                </h3>
+                <div className="line-clamp-4">
+                  <p class="md:text-sm text-gray-500 text-sm">{event.content}</p>
+                </div>
+                <div>
+                  <button
+                    onClick={() => handleDelete(event.id, index)}
+                    className="bg-red-500 hover:bg-red-700 text-white font-bold rounded p-1"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="#ffffff"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <polyline points="3 6 5 6 21 6"></polyline>
+                      <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                      <line x1="10" y1="11" x2="10" y2="17"></line>
+                      <line x1="14" y1="11" x2="14" y2="17"></line>
+                    </svg>
+                  </button>
+                  <button
+                    onClick={() => {
+                      toggleEditModal();
+                      setmodal(event);
+                    }}
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded ml-4 p-1"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="#ffffff"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path d="M20 14.66V20a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h5.34"></path>
+                      <polygon points="18 2 22 6 12 16 8 16 8 12 18 2"></polygon>
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+    {/*  */}
+      {/* <div className="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 pr-10 lg:px-8 mb-8">
         <div className="align-middle inline-block min-w-full shadow overflow-hidden bg-white shadow-dashboard px-8 pt-3 rounded-bl-lg rounded-br-lg">
           <div className="flex text-2xl font-bold text-gray-500 mb-4 justify-center items-center">
             <h2>Events</h2>
@@ -154,7 +224,7 @@ export default function AllEvent() {
             </tbody>
           </table>
         </div>
-      </div>
+      </div> */}
       <div className="flex justify-center mt-4">
         {Array.from({ length: Math.ceil(events.length / itemsPerPage) }).map(
           (_, index) => (
