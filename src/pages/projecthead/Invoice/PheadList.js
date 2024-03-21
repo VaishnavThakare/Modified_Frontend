944fc1767388cc136e0936700d8709bb0dad9a83
LokeshDC16 <lokeshdc2002@gmail.com>
2024-03-21 15:30:20 +0530

    ..

--- src/pages/projecthead/Invoice/PheadList.js
+++ src/pages/projecthead/Invoice/PheadList.js
@@ -17,71 +17,54 @@ const PheadList = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [editedItem, setEditedItem] = useState(null);
   const [editedComment, setEditedComment] = useState("");
-  const [selectedInvoicesDetails, setSelectedInvoicesDetails] = useState(null);
   const itemsPerPage = 5;
   const navigate = useNavigate();
 
-  const getInvoices=()=>{
+  const getInvoices = () => {
     axios
-    .get("https://localhost:7254/api/Invoice/All")
-    .then((response) => {
-      setInvoice(response.data); // Corrected from invoiceData to invoices
-    })
-    .catch((error) => {
-      console.error("Error fetching Invoice data:", error);
-      toast.error("Error fetching Invoice data");
-    })};
+      .get(`${process.env.REACT_APP_API_URL}/Invoice/All`)
+      .then((response) => {
+        setInvoice(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching Invoice data:", error);
+        toast.error("Error fetching Invoice data");
+      });
+  };
 
   useEffect(() => {
     getInvoices();
-    // axios
-    //   .get("https://localhost:7254/api/Invoice/All")
-    //   .then((response) => {
-    //     setInvoice(response.data); // Corrected from invoiceData to invoices
-    //   })
-    //   .catch((error) => {
-    //     console.error("Error fetching Invoice data:", error);
-    //     toast.error("Error fetching Invoice data");
-    //   });
   }, []);
-
-  
-  
 
   const currentItems = invoice.slice(
     (currentPage - 1) * itemsPerPage,
     currentPage * itemsPerPage
   );
-  
 
   const handlePrevPage = () => {
     setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
   };
-  
+
   const handleNextPage = () => {
     setCurrentPage((prevPage) =>
       Math.min(prevPage + 1, Math.ceil(invoice.length / itemsPerPage))
     );
   };
-  
 
   const handleEdit = (item) => {
     setEditedItem(item);
     setEditedComment(item.comment || "");
   };
 
-  const handleView = async (id) => {
-    try {
-      const response = await axios.get(`https://localhost:7254/api/Invoice/${id}`);
-      setSelectedInvoicesDetails(response.data);
-    } catch (error) {
-      console.error("Error fetching Invoice details:", error);
-      toast.error("Error fetching Invoice details");
-    }
+  const handleView = (id) => {
+    // console.log(id);
+    navigate(`/projecthead/invoice-details-phead/${id}`);
   };
+  
+
   const handleAccept = async (id, comment) => {
     try {
-      await axios.put(`https://localhost:7254/api/Invoice/AcceptReject/${id}`, {
+      await axios.put(`${process.env.REACT_APP_API_URL}/Invoice/AcceptReject/${id}`, {
         comment: comment,
         isAccepted: true,
       });
@@ -94,9 +77,10 @@ const PheadList = () => {
       toast.error("Error accepting Invoice item");
     }
   };
+
   const handleReject = async (id, comment) => {
     try {
-      await axios.put(`https://localhost:7254/api/Invoice/AcceptReject/${id}`, {
+      await axios.put(`${process.env.REACT_APP_API_URL}/Invoice/AcceptReject/${id}`, {
         comment: comment,
         isAccepted: false,
       });
@@ -122,14 +106,6 @@ const PheadList = () => {
     <div className="relative">
       <ToastContainer />
       <div className="overflow-x-auto mt-8 ml-2 mr-2 rounded">
-        {/* <h1 className="text-2xl font-bold mb-5 relative">
-          GRN Listing:
-          <div className="absolute bottom-0 left-0 right-0">
-            <div className="border-b border-cyan-400 w-32"></div>
-            <div className="border-b-2 border-cyan-400 mt-1"></div>
-          </div>
-        </h1> */}
-
         <table className="table-auto w-full rounded-md border-2 border-cyan-400 bg-white">
           <thead>
             <tr className="text-gray-600">
@@ -141,8 +117,8 @@ const PheadList = () => {
               <th className="px-4 py-2 text-left">Accept/Reject</th>
               <th className="px-4 py-2 text-left">ACTIONS</th>
             </tr>
-            <tr className=" text-gray-600">
-              <td colSpan="9" className=" px-4 py-1">
+            <tr className="text-gray-600">
+              <td colSpan="9" className="px-4 py-1">
                 <div style={{ borderTop: "2px solid gray" }}></div>
               </td>
             </tr>
@@ -158,38 +134,44 @@ const PheadList = () => {
                 <td className="px-4 py-2">{item.sendOn}</td>
                 <td className="px-4 py-2">
                   <button
-                    className={`py-1 px-2 rounded ${
-                      item.paymentStatus
+                    className={`py-1 px-2 rounded ${item.paymentStatus
                         ? "bg-green-200 text-green-700"
                         : "bg-red-200 text-red-600"
-                    }`}
+                      }`}
                     style={{ minWidth: "6rem" }}
                   >
                     {item.paymentStatus ? "Paid" : "Unpaid"}
                   </button>
                 </td>
-               
                 <td className="px-4 py-2">
                   <button
-                    className={`py-1 px-2 rounded ${
-                      item.isAccepted!=null
-                        ? item.isAccepted?"bg-green-200 text-green-700"
-                        : "bg-red-200 text-red-600":"bg-yello-200 text-yello-600"
-                    }`}
+                    className={`py-1 px-2 rounded ${item.isAccepted != null
+                        ? item.isAccepted
+                          ? "bg-green-200 text-green-700"
+                          : "bg-red-200 text-red-600"
+                        : "bg-yellow-200 text-yellow-600"
+                      }`}
                     style={{ minWidth: "6rem" }}
                   >
-                    {item.isAccepted !=null? item.isAccepted? "Already Accepted" : "Already Rejected" : "not accepted/Rejected"}
+                    {item.isAccepted != null
+                      ? item.isAccepted
+                        ? "Already Accepted"
+                        : "Already Rejected"
+                      : "Not Accepted/Rejected"}
                   </button>
                 </td>
+                <td className="px-4 py-2 bg-gray-50">
+                  {item.isAccepted == null ? (
+                    <button onClick={() => handleEdit(item)} className={`mr-2`}>
+                      <FontAwesomeIcon
+                        icon={faEdit}
+                        className={`text-purple-600 text-xl`}
+                      />
+                    </button>
+                  ) : (
+                    ""
+                  )}
 
-                <td className="px-4 py-2 bg-zinc-50">
-                 { item.isAccepted==null?(
-                  <button onClick={() => handleEdit(item)} className={`mr-2`}>
-                    <FontAwesomeIcon
-                      icon={faEdit}
-                      className={`text-purple-600 text-xl`}
-                    />
-                  </button>):""}
                   <button
                     onClick={() => handleView(item.id)}
                     className={`mr-2`}
@@ -229,87 +211,15 @@ const PheadList = () => {
           </tbody>
         </table>
       </div>
-      {/* Modal/Pop-up to display GRN Details */}
-      {selectedInvoicesDetails && (
-        <div className="fixed z-10 inset-0 overflow-y-auto">
-          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-            <div
-              className="fixed inset-0 transition-opacity"
-              aria-hidden="true"
-            >
-              <div className="absolute inset-0 bg-gray-500 opacity-0"></div>
-            </div>
-            <span
-              className="hidden sm:inline-block sm:align-middle sm:h-screen"
-              aria-hidden="true"
-            >
-              &#8203;
-            </span>
-            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg ">
-              <div className="bg-white px-4 pt-5 pb-4 min:p-6 min:pb-4 ">   
-                <div className="sm:flex sm:items-start ">
-                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-
-                  <h1 className="text-2xl font-bold mb-4 underline text-cyan-600">Invoice Details</h1>
-                   
-                       <p className="border-2 border-blue-500 p-4 ">
-                      <p>
-                        <span className="font-bold">Invoice No.:</span>{" "}
-                        {selectedInvoicesDetails.invoiceNo}
-                      </p>
-                      <p>
-                        <span className="font-bold">Amount:</span>{" "}
-                        {selectedInvoicesDetails.amount}
-                      </p>
-                      <p>
-                        <span className="font-bold">Sent On:</span>{" "}
-                        {selectedInvoicesDetails.sendOn}
-                      </p>
-                      <p>
-                        <span className="font-bold">Created On:</span>{" "}
-                        {selectedInvoicesDetails.createdOn}
-                      </p>
-                      </p>
-                      {selectedInvoicesDetails.documentPath && (
-                        <button
-                          className="mt-4 bg-cyan-600 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded"
-                          onClick={() =>
-                            openDocument(selectedInvoicesDetails.documentPath)
-                          }
-                        >
-                          <FontAwesomeIcon
-                            icon={faExternalLinkAlt}
-                            className="mr-2"
-                          />
-                          View Document
-                        </button>
-                      )}
-                   
-                  </div>
-                </div>
-              </div>
-              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                <button
-                  type="button"
-                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-cyan-600 text-base font-medium text-white hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500 sm:ml-3 sm:w-auto sm:text-sm"
-                  onClick={() => setSelectedInvoicesDetails(null)}
-                >
-                  Close
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
 
       <div className="flex justify-end mt-2 ml-2 mr-2">
         <table className="table-auto border-collapse rounded border-blue-500 mb-5">
           <tbody>
-            <tr className="bg-zinc-50">
+            <tr className="bg-gray-50">
               <td className="px-4 py-2" colSpan="10">
                 <button
                   onClick={handlePrevPage}
-                  className="pagination-button  bg-cyan-500 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded-3xl"
+                  className="pagination-button bg-cyan-500 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded-3xl"
                   disabled={currentPage === 1}
                 >
                   <FontAwesomeIcon
@@ -320,7 +230,7 @@ const PheadList = () => {
                 </button>
                 <button
                   onClick={handleNextPage}
-                  className="pagination-button ml-2  bg-cyan-500 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded-3xl"
+                  className="pagination-button ml-2 bg-cyan-500 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded-3xl"
                   disabled={
                     currentPage === Math.ceil(invoice.length / itemsPerPage)
                   }
@@ -341,9 +251,3 @@ const PheadList = () => {
 };
 
 export default PheadList;
-
-
-
-
-
-
