65e61e2311684aad5870d8b813e37c062e2dd601
OmkarKanade <omkarkanade8888@gmail.com>
2024-03-28 12:48:01 +0530

    Edit Project

--- src/pages/admin/ProjectEdit.js
+++ src/pages/admin/ProjectEdit.js
@@ -57,70 +57,107 @@ const ProjectEdit = () => {
   };
 
   const back = () => {
-    navigate(`/admin/projects`);
+    navigate(-1);
   };
 
   return (
-    <div className="bg-zinc-50">
-      <div className="flex text-2xl font-bold text-gray-500">
-        <h2 className="text-left text-cyan-500">EDIT PROJECT</h2>
-      </div>
-      <div className="w-1/5 bg-cyan-500 h-0.5 mb-1"></div>
-      <div className="w-1/3 bg-cyan-500 h-0.5 mb-5"></div>
-      <div className="py-10 margin-left items-center bg-zinc-50 font-poppins">
-        <div className="bg-white border-2 border-cyan-400 rounded-lg shadow-lg p-8 w-full max-w-lg ">
-          <form onSubmit={handleSubmit} className="p-1 px-10">
-            <div className="flex text-2xl font-bold text-gray-500 mb-7 justify-center items-center">
-              <h2>Edit Project Details</h2>
-            </div>
-            <div className="grid grid-cols-1 gap-4">
-              <div>
-                <label
-                  htmlFor="name"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Name
-                </label>
+    <div class="align-middle inline-block min-w-full overflow-hidden bg-zinc-50 px-8 py-3 pb-8 rounded-bl-lg rounded-br-lg">
+        <form onSubmit={handleSubmit} className="max-w-lg margin-left mt-8 appform bg-white">
+          <div className="flex text-2xl font-bold text-gray-500 mb-2 justify-center">
+          <h2 className="page-heading">Edit Project Details</h2>
+          </div>
+            <div className="mb-6 relative">
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={projectData.name}
+              onChange={handleInputChange}
+              className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              htmlFor="name"
+              className="ml-1 absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4"
+            >
+              Name
+            </label>
+          </div>
+          <div class="mb-6">
+            <div class="flex">
+              <div class="flex items-center me-4">
                 <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  value={projectData.name}
-                  onChange={handleInputChange}
-                  className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="projectStatus"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Project Status
-                </label>
-                <input
-                  type="text"
+                  id="radio-1"
+                  type="radio"
+                  value="Active"
                   name="projectStatus"
-                  id="projectStatus"
-                  value={projectData.projectStatus}
                   onChange={handleInputChange}
-                  className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  checked={projectData.projectStatus === "Active"}
+                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500"
                 />
-              </div>
-              <div>
                 <label
-                  htmlFor="description"
-                  className="block text-sm font-medium text-gray-700"
+                  for="radio-1"
+                  class="ms-2 text-sm font-medium text-gray-900"
                 >
-                  Description
+                  Active
                 </label>
-                <textarea
-                  name="description"
-                  id="description"
-                  value={projectData.description}
-                  onChange={handleInputChange}
-                  className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                />
               </div>
+              <div class="flex items-center me-4">
+                <input
+                  id="radio-2"
+                  type="radio"
+                  value="Ongoing"
+                  name="projectStatus"
+                  onChange={handleInputChange}
+                  checked={projectData.projectStatus === "Ongoing"}
+                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 "
+                />
+                <label
+                  for="radio-2"
+                  class="ms-2 text-sm font-medium text-gray-900"
+                >
+                  Ongoing
+                </label>
+              </div>
+              <div class="flex items-center me-4">
+                <input
+                  id="radio-3"
+                  type="radio"
+                  value="Done"
+                  name="projectStatus"
+                  onChange={handleInputChange}
+                  checked={projectData.projectStatus === "Done"}
+                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 "
+                />
+                <label
+                  for="radio-3"
+                  class="ms-2 text-sm font-medium text-gray-900"
+                >
+                  Done
+                </label>
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-6 relative">
+  <input
+    type="text"
+    id="description"
+    name="description"
+    value={projectData.description}
+    onChange={handleInputChange}
+    className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-500 peer"
+    placeholder=" "
+    required
+  />
+  <label
+    htmlFor="description"
+    className="ml-1 absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4"
+  >
+    Description
+  </label>
+</div>
               <div className="flex justify-center mt-4">
                 <button
                   type="submit"
@@ -136,12 +173,9 @@ const ProjectEdit = () => {
                   Back
                 </button>
               </div>
-            </div>
           </form>
           <ToastContainer />
         </div>
-      </div>
-    </div>
   );
 };
 
